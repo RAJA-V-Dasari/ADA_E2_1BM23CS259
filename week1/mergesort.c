0f87bc35d220135565cdceccce96ebe3e93d746f
Raja Vishwanath Dasari <rajavishwanath.cs23@bmsce.ac.in>
2025-05-26 21:29:43 +0530

    Update mergesort.c

--- week1/mergesort.c
+++ week1/mergesort.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-// Merge function
+#define MAX_SIZE 1000  // Set based on expected input size
+
+// Merge function using static arrays
 void merge(int arr[], int left, int mid, int right) {
     int n1 = mid - left + 1;
     int n2 = right - mid;
-    int *L = (int *)malloc(n1 * sizeof(int));
-    int *R = (int *)malloc(n2 * sizeof(int));
+
+    int L[MAX_SIZE], R[MAX_SIZE];  // Static temporary arrays
 
     for (int i = 0; i < n1; i++) L[i] = arr[left + i];
     for (int i = 0; i < n2; i++) R[i] = arr[mid + 1 + i];
@@ -20,9 +20,6 @@ void merge(int arr[], int left, int mid, int right) {
 
     while (i < n1) arr[k++] = L[i++];
     while (j < n2) arr[k++] = R[j++];
-
-    free(L);
-    free(R);
 }
 
 // Merge Sort function
@@ -34,7 +31,6 @@ void merge_sort(int arr[], int left, int right) {
         merge(arr, left, mid, right);
     }
 }
-
 
 int main() {
     int arr[] = {34, 87, 21, 65, 10, 93};  // Static array
@@ -55,4 +51,3 @@ int main() {
     printf("\n");
     return 0;
 }
-
